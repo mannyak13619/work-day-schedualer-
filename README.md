d4d9be88bb3a3d3f1578678b2d737ebf4b70581d
manny <emmanuelgomezrealpro@gmail.com>
2023-03-10 08:55:28 -0500

    added image and made updastes to css

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # work-day-schedualer-
-keeping track of day to day activities and schedule
+keeping track of day to day activities and schedule planing for the week or month ,
+on this week i used .js to give the user the ability to scheduale and with date an color coding for future past and present features .
+![Alt text](file:///Users/moneymaker13k/Desktop/code/work-day-schedualer-/index.html "schedualer")
--- script.js
+++ script.js
@@ -2,12 +2,38 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  setInterval(function () {
+
+    var today = dayjs();
+    $("#current-say").text(today.format("dddd MMM D,YYYY hh:mmA"));
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+
+      var hourBlock = parseInt($(this).data("hour"));
+
+      if (currentHour > hourBlock) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (currentHour == hourBlock) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      };
+    });
+  }, 1000)
+
+  $(".saveBtn").on("click", function () {
+    console.log($(this).siblings("textarea").val())
+    var id = $(this).parent().attr("id")
+    var input = $(this).siblings("textarea").val()
+    localStorage.setItem(id, input)
+  })
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +46,24 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("DD/MM/YYYY"))
 });
+document.getElementById("myBtn");
+ddEventListener = ("click", "dysplayDate")
+
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
